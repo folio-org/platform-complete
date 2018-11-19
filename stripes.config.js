9e726f606eec604945a8f1728d7cc03fa92b981e
John Coburn <jcoburn@ebsco.com>
2018-11-19 15:47:01 -0600

    Uncomment in stripes.config.js

--- stripes.config.js
+++ stripes.config.js
@@ -3,7 +3,7 @@ const { merge } = require('lodash');
 
 const platformComplete = {
   modules: {
-    // '@folio/calendar' : {},
+    '@folio/calendar' : {},
     '@folio/eholdings' : {},
     '@folio/finance' : {},
     '@folio/vendors' : {}
