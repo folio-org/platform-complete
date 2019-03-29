3c2f8f31c5fba21f4d5d2c9c273c6d7cb2d536d7
John Malconian <malc@indexdata.com>
2019-03-29 12:17:27 +0000

    remove all nested platforms. See STCORE-360

--- stripes.config.js
+++ stripes.config.js
@@ -1,19 +1,37 @@
-const platformCore = require('@folio/platform-core/stripes.config.js');
-const { merge } = require('lodash');
-
-const platformComplete = {
+module.exports = {
+  okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
+  config: {
+    logCategories: 'core,path,action,xhr',
+    logPrefix: '--',
+    showPerms: false
+  },
   modules: {
     '@folio/agreements': {},
+    '@folio/calendar' : {},
+    '@folio/checkin' : {},
+    '@folio/checkout' : {},
+    '@folio/circulation' : {},
     '@folio/data-import' : {},
+    '@folio/developer' : {},
     '@folio/eholdings' : {},
     '@folio/erm-usage': {},
     '@folio/finance' : {},
+    '@folio/inventory' : {},
     '@folio/licenses': {},
+    '@folio/myprofile' : {},
     '@folio/orders': {},
+    '@folio/organization' : {},
     '@folio/plugin-find-agreement': {},
+    '@folio/plugin-find-instance' : {},
     '@folio/plugin-find-license': {},
+    '@folio/plugin-find-user' : {},
     '@folio/plugin-find-vendor': {},
+    '@folio/requests' : {},
+    '@folio/search' : {},
+    '@folio/servicepoints' : {},
     '@folio/stripes-erm-components': {},
+    '@folio/tags': {},
+    '@folio/users' : {},
     '@folio/vendors': {}
   },
   branding: {
@@ -27,4 +45,3 @@ const platformComplete = {
   }
 };
 
-module.exports = merge({}, platformCore, platformComplete);
