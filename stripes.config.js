f0f869a55b091f2430ae5405a7f2ee48cacc89ef
John Malconian <malc@indexdata.com>
2019-05-08 18:58:00 +0000

    add @folio/notes. list individual platform-core and platform-erm deps.  STCOR-360

--- stripes.config.js
+++ stripes.config.js
@@ -4,12 +4,39 @@ const { merge } = require('lodash');
 
 const platformComplete = {
   modules: {
+    '@folio/agreements': {},
+    '@folio/calendar' : {},
+    '@folio/checkin' : {},
+    '@folio/checkout' : {},
+    '@folio/circulation' : {},
     '@folio/data-import': {},
+    '@folio/developer' : {},
     '@folio/eholdings' : {},
+    '@folio/erm-usage': {},
+    '@folio/inventory' : {},
     '@folio/finance' : {},
+    '@folio/licenses': {},
+    '@folio/myprofile' : {},
+    '@folio/notes' : {},
+    '@folio/orders': {},
+    '@folio/organization' : {},
     '@folio/plugin-create-item' : {},
+    '@folio/plugin-find-agreement': {},
+    '@folio/plugin-find-instance' : {},
     '@folio/plugin-find-contact' : {},
+    '@folio/plugin-find-license': {},
     '@folio/plugin-find-organization' : {},
+    '@folio/plugin-find-user' : {},
+    '@folio/plugin-find-vendor': {},
+    '@folio/requests' : {},
+    '@folio/search' : {},
+    '@folio/servicepoints' : {},
+    '@folio/stripes-erm-components': {},
+    '@folio/tags': {},
+    '@folio/tenant-settings' : {},
+    '@folio/users' : {},
+    '@folio/vendors': {}
+
   },
   branding: {
     logo: {
