7bb07ffcd84c144c8755a133a654fdc58e25dc88
John Malconian <malc@indexdata.com>
2019-05-09 02:14:26 +0000

    remove plugin-ui-find-vendor

--- stripes.config.js
+++ stripes.config.js
@@ -1,42 +1,50 @@
-const platformCore = require('@folio/platform-core/stripes.config.js');
-const platformERM = require('@folio/platform-erm/stripes.config.js');
-const { merge } = require('lodash');
+// const platformCore = require('@folio/platform-core/stripes.config.js');
+// const platformERM = require('@folio/platform-erm/stripes.config.js');
+// const { merge } = require('lodash');
 
-const platformComplete = {
+module.exports = {
+  okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
+  config: {
+    logCategories: 'core,path,action,xhr',
+    logPrefix: '--',
+    showPerms: false
+  },
+
   modules: {
-    '@folio/agreements': {},
+    '@folio/agreements' : {},
     '@folio/calendar' : {},
     '@folio/checkin' : {},
     '@folio/checkout' : {},
     '@folio/circulation' : {},
-    '@folio/data-import': {},
+    '@folio/data-import' : {},
     '@folio/developer' : {},
     '@folio/eholdings' : {},
-    '@folio/erm-usage': {},
-    '@folio/inventory': {},
-    '@folio/finance': {},
-    '@folio/licenses': {},
-    '@folio/myprofile': {},
-    '@folio/notes': {},
-    '@folio/orders': {},
-    '@folio/organization': {},
-    '@folio/plugin-create-item': {},
-    '@folio/plugin-find-agreement': {},
-    '@folio/plugin-find-instance': {},
-    '@folio/plugin-find-contact': {},
-    '@folio/plugin-find-license': {},
-    '@folio/plugin-find-organization': {},
-    '@folio/plugin-find-user': {},
-    '@folio/plugin-find-vendor': {},
-    '@folio/requests': {},
-    '@folio/search': {},
+    '@folio/erm-usage' : {},
+    '@folio/inventory' : {},
+    '@folio/finance' : {},
+    '@folio/licenses' : {},
+    '@folio/myprofile' : {},
+    '@folio/notes' : {},
+    '@folio/orders' : {},
+    '@folio/organization' : {},
+    '@folio/organizations' : {},
+    '@folio/plugin-create-item' : {},
+    '@folio/plugin-find-agreement' : {},
+    '@folio/plugin-find-instance' : {},
+    '@folio/plugin-find-contact' : {},
+    '@folio/plugin-find-license' : {},
+    '@folio/plugin-find-organization' : {},
+    '@folio/plugin-find-user' : {},
+    '@folio/requests' : {},
+    '@folio/search' : {},
     '@folio/servicepoints' : {},
-    '@folio/stripes-erm-components': {},
-    '@folio/tags': {},
+    '@folio/stripes-erm-components' : {},
+    '@folio/tags' : {},
     '@folio/tenant-settings' : {},
     '@folio/users' : {},
     '@folio/vendors': {}
   },
+
   branding: {
     logo: {
       src: './tenant-assets/opentown-libraries-logo.png',
@@ -45,7 +53,7 @@ const platformComplete = {
     favicon: {
       src: './tenant-assets/folio-favicon.png',
     },
-  }
+  },
 };
 
-module.exports = merge({}, platformCore, platformERM, platformComplete);
+// module.exports = merge({}, platformCore, platformERM, platformComplete);
