01fddd932e82c9d1e981b9d72e97b8b1669bd99e
Dmytro-Melnyshyn <Dmytro_Melnyshyn@epam.com>
2023-09-27 17:41:47 +0300

    add circulation setting

--- stripes.config.js
+++ stripes.config.js
@@ -9,6 +9,7 @@ module.exports = {
   modules: {
     '@folio/agreements' : {},
     '@folio/bulk-edit': {},
+    '@folio/circulation' : {},
     '@folio/consortia-settings' : {},
     '@folio/courses' : {},
     '@folio/data-import' : {},
