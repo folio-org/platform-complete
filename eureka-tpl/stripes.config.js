bdee5f2f22184d77c75ab19d7a2d7aaaec07e95b
Noah Overcash <novercash@ebsco.com>
2024-10-25 10:45:21 -0400

    latest

--- eureka-tpl/stripes.config.js
+++ eureka-tpl/stripes.config.js
@@ -1,19 +1,28 @@
-// const platformCore = require('@folio/platform-core/stripes.config.js');
-// const platformERM = require('@folio/platform-erm/stripes.config.js');
-// const { merge } = require('lodash');
-
 module.exports = {
-  okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
+  okapi: {
+    'url': '${kongUrl}',
+    'uiUrl': '${tenantUrl}',
+    'authnUrl': '${keycloakUrl}'
+  },
   config: {
+    hasAllPerms: ${hasAllPerms},
+    useSecureTokens: true,
     logCategories: 'core,path,action,xhr',
     logPrefix: '--',
     maxUnpagedResourceCount: 2000,
     showPerms: false,
-    preserveConsole: true,
-    useSecureTokens: true,
+    isSingleTenant: ${isSingleTenant},
+    isEureka: true,
+    tenantOptions: ${tenantOptions},
+    rtr: {
+      idleSessionTTL: '1h',
+      idleModalTTL: '30s',
+    }
   },
-
-  modules: {
+modules: {
+    '@folio/authorization-roles': {},
+    '@folio/authorization-policies': {},
+    '@folio/plugin-select-application': {},
     '@folio/acquisition-units': {},
     '@folio/agreements' : {},
     '@folio/bulk-edit': {},
@@ -83,16 +92,13 @@ module.exports = {
     '@folio/tenant-settings' : {},
     '@folio/users' : {}
   },
-
   branding: {
     logo: {
       src: './tenant-assets/opentown-libraries-logo.png',
-      alt: 'Opentown Libraries',
+      alt: 'Opentown Libraries'
     },
     favicon: {
-      src: './tenant-assets/folio-favicon.png',
+      src: './tenant-assets/folio-favicon.png'
     },
-  },
+  }
 };
-
-// module.exports = merge({}, platformCore, platformERM, platformComplete);
