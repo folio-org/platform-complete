c950eb872f3da3564727e65512e5071a47b69a01
Dmytro Melnyshyn <dmytro_melnyshyn@epam.com>
2025-05-27 16:34:38 +0300

    remove @folio/claims from eureka-tpl

--- eureka-tpl/stripes.config.js
+++ eureka-tpl/stripes.config.js
@@ -31,7 +31,6 @@ module.exports = {
     '@folio/checkout' : {},
     '@folio/circulation' : {},
     '@folio/circulation-log' : {},
-    '@folio/claims' : {},
     '@folio/courses' : {},
     '@folio/data-import' : {},
     '@folio/data-export' : {},
