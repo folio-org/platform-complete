890193b3b2eae4bff37b877aa5c712a128cc457c
Dmytro Melnyshyn <dmytro_melnyshyn@epam.com>
2025-05-27 16:40:16 +0300

    remove @folio/gobi-settings from eureka-tpl

--- eureka-tpl/stripes.config.js
+++ eureka-tpl/stripes.config.js
@@ -41,7 +41,6 @@ module.exports = {
     '@folio/erm-usage' : {},
     '@folio/export-manager': {},
     '@folio/handler-stripes-registry': {},
-    "@folio/gobi-settings": {},
     '@folio/inn-reach' : {},
     '@folio/inventory' : {},
     '@folio/invoice' : {},
