d4536823b582b2e19f079f9ee9de9620a7ce5dc9
Wayne Schneider <wayne@indexdata.com>
2025-12-11 11:56:29 -0600

    Updates based on Bugfest testing and feedback (#3511)
    * Remove mod-marc-migrations
    * Update mod-reporting to v1.4.2
    * Remove mod-mosaic and @folio/mosaic-settings

--- stripes.config.js
+++ stripes.config.js
@@ -39,7 +39,6 @@ module.exports = {
     '@folio/lists': {},
     '@folio/local-kb-admin': {},
     '@folio/marc-authorities': {},
-    '@folio/mosaic-settings': {},
     '@folio/myprofile' : {},
     '@folio/notes' : {},
     '@folio/oai-pmh' : {},
